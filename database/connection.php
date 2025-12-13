71e7c26b2c7e22fd59b500e6fb6e15c7f196ff0f
Steve <swaller111@gmail.com>
2025-12-13 14:54:22 +0100

    Cleanup

---
+++ database/connection.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * 
+	 * Es la instancia de la conexion a la base de datos
+	 * @db object
+	 * 
+	 */
+
+// require('/home/metmeetings/private/essential_string.php');
+
+if (defined('MET_ENV') && constant('MET_ENV') == 'LOCAL') {
+    $db = new DatabaseConnection("localhost", "root", "", "met");
+}else{
+    $db=new DatabaseConnection("localhost:3308", "maindbuser9634", $essential_string, "metmeetings_edatamet");
+}
+
+$db->connect();
+	
+//Definimos el acronimo de la base de datos
+define("OBJECT_DB_ACRONYM","ed");
